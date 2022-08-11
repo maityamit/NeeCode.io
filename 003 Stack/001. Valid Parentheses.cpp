ce9f99c6087e36d75d8c06585e3db21af5e75ae2
Amit Maity <maityamit308@gmail.com>
2022-08-11 12:36:47 +0530

    Update 001. Valid Parentheses.cpp

--- 003 Stack/001. Valid Parentheses.cpp
+++ 003 Stack/001. Valid Parentheses.cpp
@@ -1,1 +1,32 @@
 
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        bool flag = false;
+        for(int i=0;i<s.length();i++){
+            if(s[i]=='(' || s[i]=='{' || s[i]=='['){
+                st.push(s[i]);
+                flag = false;
+            } else if(s[i]==')' && st.empty()==false && st.top()=='(' ){
+                st.pop();
+                flag = true;
+            }else if(s[i]=='}'&& st.empty()==false && st.top()=='{' ){
+                st.pop();
+                flag=true;
+            }else if(s[i]==']'&& st.empty()==false && st.top()=='[' ){
+                st.pop();
+                flag = true;
+            }else{
+                flag=false;
+                break;
+            }
+        }
+        if(st.empty()==false){
+            return false;
+        }else{
+         return flag;   
+        }
+        
+    }
+};
